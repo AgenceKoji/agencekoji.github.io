6b1334a12f6c4421e6a5fa543206c3c5ab7dfc0d
anthonyvd <anthony@agencekoji.com>
2025-10-15 12:46:43 -0400

    Mobile landing page

--- src/components/LandingView.vue
+++ src/components/LandingView.vue
@@ -12,24 +12,27 @@ export default {
 
 <template>
   <LandingHeader />
-  <div class="flex flex-col mx-auto max-w-[1080px] pt-12 text-xl">
+  <div class="flex flex-col mx-auto max-w-[1080px] pt-12 text-xl gap-12">
     <h2
-      class="text-center text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl dark:text-white pb-24"
+      class="text-center text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl dark:text-white lg:pb-12 px-6"
     >
       Découvrez des sakés exceptionnels
     </h2>
-    <div class="flex gap-12 items-center justify-start h-64">
-      <img class="object-contain h-full" src="/pouring_sake.jpg" />
-      <p>
+
+    <div class="flex gap-12 items-center justify-start lg:h-64 flex-wrap lg:flex-nowrap">
+      <img class="object-contain w-full lg:h-full" src="/pouring_sake.jpg" />
+      <p class="px-6 text-center lg:text-start">
         Agence Koji est une jeune agence de représentation de produits d'alcool, mettant l'emphase
         sur les sakés d'artisant. Notre mission est de faire découvrir des sakés d'exception aux
         Québécois.
       </p>
     </div>
 
-    <div class="flex flex-row-reverse gap-12 items-center justify-start h-64">
-      <img class="object-contain h-full" src="/sake_cups.jpg" />
-      <p>
+    <div
+      class="flex flex-row-reverse gap-12 items-center justify-start lg:h-64 flex-wrap lg:flex-nowrap"
+    >
+      <img class="object-contain w-full lg:h-full" src="/sake_cups.jpg" />
+      <p class="px-6 text-center lg:text-end">
         Nous travaillons avec des petits brasseurs indépendents pour vous présenter des produits
         artisanaux et de petites production, tout en couvrant la vaste diversité de styles qu'offre
         le saké.
